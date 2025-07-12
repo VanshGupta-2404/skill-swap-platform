ec067a2d1b17b96608ed9c4bea0637efd3c7a00f
Vansh Gupta <guptavansh2404@gmail.com>
2025-07-12 10:06:04 +0530

    Initial folder structure for backend and frontend

---
+++ backend/skill-swap-api/skill-swap-api/src/test/java/com/skillswap/skill_swap_api/SkillSwapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillswap.skill_swap_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
